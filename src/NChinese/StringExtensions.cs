ec6a2a24fb086c07d38f5984c5a6d2f03d6ded61
Michael Tsai <huanlin.tsai@gmail.com>
2018-04-07 02:03:51 +0800

    Initial commit

---
+++ src/NChinese/StringExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace NChinese
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 判斷傳入的字元是否為ㄅㄆㄇㄈ（注意不含讀音符號）。
+        /// </summary>
+        /// <param name="aChar">類行為 String 的中文字元（unicode 字元不能以 char）</param>
+        /// <returns></returns>
+        public static bool IsBopomofo(this string aChar)
+        {
+            if (String.IsNullOrEmpty(aChar))
+                return false;
+            int code = Char.ConvertToUtf32(aChar, 0);
+            if (code >= 0x3105 && code <= 0x3129) // ㄅㄆㄇㄈ
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// 判斷傳入的字元是否為中日韓表意文字（CJK Ideographs；aka Unihan）。注意不含注音符號ㄅㄆㄇㄈ。
+        /// </summary>
+        /// <param name="aChar"></param>
+        /// <returns></returns>
+        public static bool IsUnihan(this string aChar)
+        {
+            if (String.IsNullOrEmpty(aChar))
+                return false;
+            int code = Char.ConvertToUtf32(aChar, 0);
+            if (code >= 0x4e00 && code <= 0x9fff) // CJK Unified Ideographs （中日韓統一表意文字）
+                return true;
+            else if (code >= 0x3400 && code <= 0x4dbf)   // CJK Unified Ideographs Extension A
+                return true;
+            else if (code >= 0x20000 && code <= 0x2a6df) // CJK Unified Ideographs Extension B
+                return true;
+            else if (code >= 0x2a700 && code <= 0x2b73f) // CJK Unified Ideographs Extension C
+                return true;
+            else if (code >= 0x2b740 && code <= 0x2b81f) // CJK Unified Ideographs Extension D
+                return true;
+            else if (code >= 0x2b820 && code <= 0x2ceaf) // CJK Unified Ideographs Extension E
+                return true;
+            else if (code >= 0x2ceb0 && code <= 0x2ebef) // CJK Unified Ideographs Extension F
+                return true;
+            else if (code >= 0xf900 && code <= 0xfaff) // 中日韓兼容表意文字
+                return true;
+            return false;
+        }
+
+    }
+}
---
+++ test/NChinese.Test/StringExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+using SharpChinese;
+
+namespace ChineseTest
+{
+    /// <summary>
+    /// 注意：此檔案包含 UTF-16 編碼的中文字，故存檔時必須採用 Unitcode 編碼格式（即 UTF-16 Little Endian）。
+    /// </summary>
+    [TestFixture]
+    public class StringExtensionsTests
+    {
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [TestCase("王𤭢𪚥誤吳吴")]
+        [TestCase("𠑗㒨𨉼")] // CJK 擴展 B 區
+        public void Should_IsUnihan_Return_True_For_CJK_Ideographs(string s)
+        {
+            var charEnum = StringInfo.GetTextElementEnumerator(s);
+            while (charEnum.MoveNext())
+            {
+                Assert.IsTrue(charEnum.GetTextElement().IsUnihan());
+            }           
+        }
+
+        [TestCase("2%-*")]
+        [TestCase("ㄅㄆㄇㄈ")] // 注音符號不算表意文字
+        [TestCase("【」〇")]   // 中文全形符號
+        [TestCase("あえかァィ")]   // 日文平假名和片假名
+        public void Should_IsUnihan_Return_False_For_Non_CJK_Ideographs(string s)
+        {
+            var charEnum = StringInfo.GetTextElementEnumerator(s);
+            while (charEnum.MoveNext())
+            {
+                Assert.IsFalse(charEnum.GetTextElement().IsUnihan());
+            }
+        }
+
+    }
+}
