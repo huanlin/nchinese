823ab6e4a026f7767f3295e48863e737d962bd9a
Michael Tsai <huanlin.tsai@gmail.com>
2018-10-11 20:34:30 +0800

    Upgrade Nuke to v0.10.5 and change projects' target framework from 4.5.2 to 4.7.1.

--- build/Build.cs
+++ build/Build.cs
@@ -1,63 +1,69 @@
-﻿using Nuke.Common.Tools.GitVersion;
-using Nuke.Common.Tools.NuGet;
+using System;
+using System.Linq;
 using Nuke.Common;
-using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
-using static Nuke.Common.Tools.NuGet.NuGetTasks;
+using Nuke.Common.Git;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
-    // Console application entry. Also defines the default target.
     public static int Main () => Execute<Build>(x => x.Compile);
 
-    // Auto-injection fields:
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly string Configuration = IsLocalBuild ? "Debug" : "Release";
 
-    [Nuke.Common.Tools.GitVersion.GitVersion] readonly GitVersion GitVersion;
-    // Semantic versioning. Must have 'GitVersion.CommandLine' referenced.
+    [Solution("src/NChinese.sln")] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
 
-    // [GitRepository] readonly GitRepository GitRepository;
-    // Parses origin, branch name and head from git config.
-
-    // [Parameter] readonly string MyGetApiKey;
-    // Returns command-line arguments and environment variables.
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath TestsDirectory => RootDirectory / "tests";
+    AbsolutePath OutputDirectory => RootDirectory / "output";
 
     Target Clean => _ => _
-            .OnlyWhen(() => false) // Disabled for safety.
-            .Executes(() =>
-            {
-                DeleteDirectories(GlobDirectories(SourceDirectory, "**/bin", "**/obj"));
-                EnsureCleanDirectory(OutputDirectory);
-            });
+        .Executes(() =>
+        {
+            DeleteDirectories(GlobDirectories(SourceDirectory, "**/bin", "**/obj"));
+            DeleteDirectories(GlobDirectories(TestsDirectory, "**/bin", "**/obj"));
+            EnsureCleanDirectory(OutputDirectory);
+        });
 
     Target Restore => _ => _
-            .DependsOn(Clean)
-            .Executes(() =>
-            {
-                MSBuild(s => DefaultMSBuildRestore);
-            });
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
 
     Target Compile => _ => _
-            .DependsOn(Restore)
-            .Executes(() =>
-            {
-                MSBuild(s => DefaultMSBuildCompile);
-            });
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.GetNormalizedAssemblyVersion())
+                .SetFileVersion(GitVersion.GetNormalizedFileVersion())
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .EnableNoRestore());
+        });
 
     Target Pack => _ => _
-            .DependsOn(Compile)
-            .Executes(() =>
-            {
-                Logger.Info($"Nuget packages will be created in '{OutputDirectory}'");
-
-                var nugetSettings = DefaultNuGetPack.SetBasePath(RootDirectory);
-                string nuspecFileName = RootDirectory / $"nuspec/NChinese.nuspec";
-                Logger.Info($"Creating Nuget package with {nuspecFileName}");
-                NuGetPack(nuspecFileName, s => nugetSettings);
-/*
-                nuspecFileName = RootDirectory / $"nuspec/NChinese.Imm.nuspec";
-                Logger.Info($"Creating Nuget package with {nuspecFileName}");
-                NuGetPack(nuspecFileName, s => nugetSettings);
-*/
-            });
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+                .SetProject(Solution)
+                .SetVersion(GitVersion.NuGetVersionV2)
+                .SetOutputDirectory(OutputDirectory)
+                .SetConfiguration(Configuration)
+                .EnableNoBuild()
+                .EnableIncludeSymbols());
+        });
 }
--- samples/ReverseConversionDemo/Form1.Designer.cs
+++ samples/ReverseConversionDemo/Form1.Designer.cs
@@ -33,10 +33,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.ZhuyinFromBuiltInDict_TextBox = new System.Windows.Forms.TextBox();
-            this.PinyinFromImmApi_TextBox = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.ZhuyinFromPinyin_TextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // InputTextBox
@@ -73,58 +69,22 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(31, 95);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(297, 27);
+            this.label2.Size = new System.Drawing.Size(107, 27);
             this.label2.TabIndex = 4;
-            this.label2.Text = "使用內建詞庫所查詢的注音字根：";
+            this.label2.Text = "注音字根：";
             // 
             // ZhuyinFromBuiltInDict_TextBox
             // 
-            this.ZhuyinFromBuiltInDict_TextBox.Location = new System.Drawing.Point(334, 95);
+            this.ZhuyinFromBuiltInDict_TextBox.Location = new System.Drawing.Point(144, 92);
             this.ZhuyinFromBuiltInDict_TextBox.Name = "ZhuyinFromBuiltInDict_TextBox";
-            this.ZhuyinFromBuiltInDict_TextBox.Size = new System.Drawing.Size(494, 32);
+            this.ZhuyinFromBuiltInDict_TextBox.Size = new System.Drawing.Size(574, 32);
             this.ZhuyinFromBuiltInDict_TextBox.TabIndex = 5;
-            // 
-            // PinyinFromImmApi_TextBox
-            // 
-            this.PinyinFromImmApi_TextBox.Location = new System.Drawing.Point(334, 146);
-            this.PinyinFromImmApi_TextBox.Name = "PinyinFromImmApi_TextBox";
-            this.PinyinFromImmApi_TextBox.Size = new System.Drawing.Size(494, 32);
-            this.PinyinFromImmApi_TextBox.TabIndex = 7;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 146);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(297, 27);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "使用微軟輸入法 API 反查的拼音：";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(31, 202);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(278, 27);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "將上面查到的拼音轉換成注音：";
-            // 
-            // ZhuyinFromPinyin_TextBox
-            // 
-            this.ZhuyinFromPinyin_TextBox.Location = new System.Drawing.Point(334, 199);
-            this.ZhuyinFromPinyin_TextBox.Name = "ZhuyinFromPinyin_TextBox";
-            this.ZhuyinFromPinyin_TextBox.Size = new System.Drawing.Size(494, 32);
-            this.ZhuyinFromPinyin_TextBox.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(888, 264);
-            this.Controls.Add(this.ZhuyinFromPinyin_TextBox);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.PinyinFromImmApi_TextBox);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(756, 169);
             this.Controls.Add(this.ZhuyinFromBuiltInDict_TextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -147,10 +107,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox ZhuyinFromBuiltInDict_TextBox;
-        private System.Windows.Forms.TextBox PinyinFromImmApi_TextBox;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox ZhuyinFromPinyin_TextBox;
     }
 }
 
